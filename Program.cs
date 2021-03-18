485f51dd8374a455162f902f641a79f5ea469b5a
marcellof23 <01faria.marcello@gmail.com>
2021-03-18 22:30:03 +0700

    add BFS friend rec but not yet sorted by mutual

--- Program.cs
+++ Program.cs
@@ -3,76 +3,218 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-class Graph
+
+public class Vertex
 {
-
-    // No. of vertices	 
-    private int vertex;
-    public int TotalVertex { get { return adjList.Length; } }
-    public int TotalEachV(int i) { return adjList[i].Count; }
-    //Adjacency Lists 
-    List<int>[] adjList;
-    public Graph(int V)
+    public string value;
+    public List<string> edges;
+    public Vertex(string _value)
     {
-        adjList = new List<int>[V];
-        for (int i = 0; i < TotalVertex; i++)
+        this.value = _value;
+        this.edges = new List<string>();
+    }
+}
+
+public class Graph
+{
+    private int numVertices;
+    private List<Vertex> vertices;
+    public Graph(int _numVertices)
+    {
+        this.numVertices = _numVertices;
+        this.vertices = new List<Vertex>();
+    }
+
+    private void addVertex(string c)
+    {
+        if (findVertex(c) == null)
         {
-            adjList[i] = new List<int>(5);
+            vertices.Add(new Vertex(c));
         }
-        vertex = V;
+        return;
     }
 
-    public void AddEdge(int v, int w)
+    private Vertex findVertex(string c)
     {
-        adjList[v].Add(w);
-        adjList[w].Add(v);
-        adjList[v].Sort();
-        adjList[w].Sort();
+        return vertices.Find(v => v.value == c);
     }
 
-    public void BFS(int source)
+    private int findVertexIdx(string c)
     {
+        for (int i = 0; i < numVertices; i++)
+        {
+            if (vertices[i].value == c)
+            {
+                return i;
+            }
+        }
+        return -1;
+    }
 
-        bool[] vis = Enumerable.Repeat((bool)false, vertex).ToArray();
+    public void addEdge(string src, string dest)
+    {
+        addVertex(src);
+        addVertex(dest);
 
-        vis[source] = true;
+        Vertex vsrc = findVertex(src);
+        Vertex vdest = findVertex(dest);
 
-        List<int> L = new List<int>();
+        vsrc.edges.Add(dest);
+        vdest.edges.Add(src);
+
+        vsrc.edges.Sort();
+        vdest.edges.Sort();
+    }
+    public void print()
+    {
+        for (int i = 0; i < vertices.Count; i++)
+        {
+            Console.Write("({0})-", vertices[i].value);
+            foreach (string edge in vertices[i].edges)
+            {
+                Console.Write("{0} ", edge);
+            }
+            Console.WriteLine();
+        }
+    }
+    public void friendRecommendationBFS(string source)
+    {
+        string AkunSource = source;
+        bool[] vis = new bool[numVertices];
+        int[] level = new int[numVertices];
+        int i = 0;
+        foreach (Vertex v in vertices)
+        {
+            vis[findVertexIdx(v.value)] = false;
+            level[findVertexIdx(v.value)] = 0;
+        }
+
+        vis[findVertexIdx(source)] = true;
+        level[findVertexIdx(source)] = 0;
+        List<string> L = new List<string>();
+        List<string> Friend = new List<string>();
         L.Add(source);
 
         while (L.Any())
         {
             source = L.First();
-            Console.Write(source);
-            Console.Write(" ");
             L.RemoveAt(0);
 
-            List<int> list = adjList[source];
-
-            foreach (var item in list)
+            foreach (string edge in vertices[findVertexIdx(source)].edges)
             {
-                if (!vis[item])
+                if (!vis[findVertexIdx(edge)])
                 {
-                    vis[item] = true;
-                    L.Add(item);
+                    vis[findVertexIdx(edge)] = true;
+                    level[findVertexIdx(edge)] = level[findVertexIdx(source)] + 1;
+                    L.Add(edge);
+                    if (level[findVertexIdx(edge)] == 2)
+                    {
+                        Friend.Add(edge);
+                    }
                 }
             }
+            i++;
         }
         Console.Write("\n");
+        Console.Write("\n");
+        Console.Write("Daftar rekomendasi teman untuk akun {0}:  \n", AkunSource);
+        while (Friend.Any())
+        {
+            string s = Friend.First();
+            Friend.RemoveAt(0);
+            Console.Write("Nama akun: {0}  ", s);
+            int mutual = 0;
+            List<string> Rec = new List<string>();
+            foreach (string edge in vertices[findVertexIdx(s)].edges)
+            {
+                if (level[findVertexIdx(edge)] == 1)
+                {
+                    Rec.Add(edge);
+                    mutual++;
+                }
+            }
+            Console.WriteLine(" ");
+            Console.WriteLine("mutual {0}", mutual.ToString());
+            foreach (string Recommendation in Rec)
+            {
+                Console.WriteLine("{0}", Recommendation);
+            }
+        }
     }
-
-    static void Main(string[] args)
+    public void exploreFriendsDFS(string src, string dest)
     {
-        Graph G = new Graph(10);
-        G.AddEdge(1, 0);
-        G.AddEdge(1, 2);
-        G.AddEdge(0, 4);
-        G.AddEdge(0, 3);
-        G.AddEdge(2, 7);
-        G.AddEdge(2, 8);
-        G.AddEdge(1, 6);
-        Console.Write("Contoh bfs dari 2 : \n");
-        G.BFS(1);
+        bool[] visited = new bool[numVertices];
+        foreach (Vertex v in vertices)
+        {
+            visited[findVertexIdx(v.value)] = false;
+        }
+        List<string> path = new List<string>();
+        dfs(src, dest, visited, path);
+        if (visited[findVertexIdx(dest)] == false)
+        {
+            Console.WriteLine("Tidak ada jalur koneksi yang tersedia\nAnda harus memulai koneksi baru itu sendiri.");
+        }
+
+    }
+    private void printPath(List<string> path)
+    {
+        for (int i = 0; i < path.Count - 1; i++)
+        {
+            Console.Write("{0} -> ", path[i]);
+        }
+        Console.Write("{0}", path[path.Count - 1]);
+        Console.WriteLine("\n{0} degree connection.", (path.Count) - 2);
+    }
+    private void dfs(string src, string dest, bool[] visited, List<string> path)
+    {
+        path.Add(src);
+        if (src == dest)
+        {
+            printPath(path);
+            visited[findVertexIdx(dest)] = true;
+            return;
+        }
+        visited[findVertexIdx(src)] = true;
+        if (visited[findVertexIdx(dest)] == false)
+        {
+            foreach (string edge in vertices[findVertexIdx(src)].edges)
+            {
+                if (visited[findVertexIdx(edge)] == false)
+                {
+                    dfs(edge, dest, visited, path);
+                }
+            }
+            path.RemoveAt(path.Count - 1);
+        }
     }
 }
 
+class Program
+{
+    static void Main(string[] args)
+    {
+        Graph g = new Graph(13);
+        g.addEdge("alif", "bhadrika");
+        g.addEdge("karel", "renaldi");
+        g.addEdge("karel", "bhadrika");
+        g.addEdge("rila", "karel");
+        g.addEdge("karel", "mandala");
+        g.print();
+        g.exploreFriendsDFS("karel", "alif");
+        Graph G = new Graph(13);
+        G.addEdge("A", "B");
+        G.addEdge("A", "C");
+        G.addEdge("A", "D");
+        G.addEdge("B", "C");
+        G.addEdge("B", "E");
+        G.addEdge("B", "F");
+        G.addEdge("C", "F");
+        G.addEdge("C", "G");
+        G.addEdge("D", "G");
+        G.addEdge("D", "F");
+        G.addEdge("E", "H");
+        G.addEdge("E", "F");
+        G.addEdge("F", "H");
+        G.friendRecommendationBFS("A");
+    }
+}
